c2c2896c0060362f50cf05cb4ab746e4b59faaa2
hanyong5 <hanyong5@naver.com>
2022-07-11 14:27:28 +0900

    toggle v2

--- 20220711_togglemenu/js/script.js
+++ 20220711_togglemenu/js/script.js
@@ -1,1 +1,11 @@
-console.log("hi");
+
+let view = true;
+$(".toggleMenu").click(function(){
+    if( view == true){
+        $(".topMenu").addClass("active");
+        view = false;
+    }else{
+        $(".topMenu").removeClass("active");
+        view = true;
+    }
+})
