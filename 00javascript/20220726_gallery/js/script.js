55b644e2d25edef1e6e2113362d3423d292bcd91
hanyong5 <hanyong5@naver.com>
2022-07-26 12:20:14 +0900

    gallery v2

--- 00javascript/20220726_gallery/js/script.js
+++ 00javascript/20220726_gallery/js/script.js
@@ -1,1 +1,26 @@
-console.log("ok");
+$(".itemWrap>li").click(function(){
+    console.log($(this).index());
+    let i = $(this).index();
+    $("#image>p").hide();
+    $("#image>p").eq(i).show();
+})
+
+let current = 0;
+$(".right").click(function(){
+    current++;
+    if(current == 3) { current = 2 }
+    console.log(current);
+    view(current)
+})
+
+$(".left").click(function(){
+    current--;
+    if(current < 0){current = 0}
+    view(current)
+})
+
+function view(current){
+    let value = current * -760;
+    // $(".itemWrap").css("left",value)
+    $(".itemWrap").stop().animate({"left":value},300)
+}
