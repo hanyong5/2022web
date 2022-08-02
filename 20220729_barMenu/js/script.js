5c825e1e44cc29d77a9ee0574cbb1818912b532d
hanyong5 <hanyong5@naver.com>
2022-08-02 11:34:19 +0900

    barmenu v2

--- 20220729_barMenu/js/script.js
+++ 20220729_barMenu/js/script.js
@@ -7,8 +7,24 @@
 // });
 
 
-$(".navbar ul").find("li").mouseover(over);
-$(".navbar ul").mouseout(out);
+// menu 0 1 2 3 4
+function nowMenu(menu){
+    console.log(menu)
+
+    if(menu == "main"){
+        $(".navbar ul").find("li").mouseover(over);
+        $(".navbar ul").mouseout(out);
+    }else{
+        $(".navbar li").eq(menu).addClass("active");
+        $(".bar").css({left:menu*240,opacity:1});
+        $(".navbar ul").find("li").mouseover(over);
+        $(".navbar ul").mouseout(function(){
+            $(".bar").css({left:menu*240,opacity:1});
+        });
+    }
+
+}
+
 
 function over(){
     let i = $(this).index();
