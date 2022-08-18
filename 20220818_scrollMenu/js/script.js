f5ff46e6e17c7c9f5342f2e84d94fc9cc48fee0e
hanyong5 <hanyong5@naver.com>
2022-08-18 13:58:15 +0900

    scrollmenu v3

--- 20220818_scrollMenu/js/script.js
+++ 20220818_scrollMenu/js/script.js
@@ -28,6 +28,37 @@ let threeTop = $("#v03").offset().top;
 $(".menuWrap > a").click(function(e){
     e.preventDefault();
     let i = $(this).index();
+    $(".menuWrap a").removeClass("active").eq(i).addClass("active");
     let st = $(".scene").eq(i).offset().top;
     $("html,body").stop().animate({scrollTop:st})
 });
+
+
+$(window).on("scroll",function(){
+    // let scrollY = $(window).scrollTop();
+    let scrollY = window.pageYOffset;
+    console.log(scrollY)
+    // if(scrollY >= oneTop)
+    // if(oneTop <= scrollY){
+    //     $(".menuWrap a").removeClass("active").eq(0).addClass("active");
+    // }
+    // if(twoTop <= scrollY){
+    //     $(".menuWrap a").removeClass("active").eq(1).addClass("active");
+    // }
+    // if(threeTop <= scrollY){
+    //     $(".menuWrap a").removeClass("active").eq(2).addClass("active");
+    // }
+
+    // for(let i=0;i<$(".scene").length;i++){
+    //     if( $(".scene").eq(i).offset().top <= scrollY){
+    //         $(".menuWrap a").removeClass("active").eq(i).addClass("active");
+    //     }
+    // }
+
+    // Array.forEach(function(item){})
+    $(".scene").each(function(index){
+            if( $(this).offset().top <= scrollY){
+                $(".menuWrap a").removeClass("active").eq(index).addClass("active");
+            }
+    })
+})
