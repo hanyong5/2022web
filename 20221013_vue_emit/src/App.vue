5ba10728dd504a0a20dcde886c114a395d356165
hanyong5 <hanyong5@naver.com>
2022-10-13 12:04:34 +0900

    vue emit

---
+++ 20221013_vue_emit/src/App.vue
@@ -0,0 +1,62 @@
+<template>
+  <web-banner :banData="bData" @modalOpen="modalView"></web-banner>
+
+
+  <div class="start" :class="{end:mView}">
+      <!-- com  -->
+    <div class="modal" :class="{'d-block':mView}" >
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title">{{bData[mData].name}}</h5>
+              <button type="button" class="btn-close" @click="mView=false"></button>
+            </div>
+            <div class="modal-body">
+              <img :src="`./images/directs/${bData[mData].src}`" alt="">
+              <p>Modal body text goes here.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- com -->
+  </div>
+
+</template>
+
+<script>
+import bData from "@/data/directs.js";
+import WebBanner from "@/components/WebBanner.vue";
+
+export default {
+  components: {
+    WebBanner,
+  },
+  data() {
+    return {
+      bData: bData,
+      mView:false,
+      mData:1,
+    };
+  },
+  methods:{
+    modalView(num,name){
+      console.log(num,name);
+      this.mView = true;
+      this.mData = num;
+    }
+  }
+
+};
+</script>
+
+<style lang="scss">
+@import url(./assets/style.css);
+.start{
+  opacity: 0;
+  transition:0.3s;
+  &.end{
+    opacity: 1;
+  }
+}
+
+</style>
---
+++ 20221013_vue_emit/src/components/WebBanner.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-6 col-md-3 col-lg" v-for="(item,i) in banData" :key="i">
+        <img :src="`./images/directs/${item.src}`" alt="" class="w-100">
+        <div class="txt text-center" @click="$emit('modalOpen',i,item.name)">
+          {{item.name}}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    props:["banData"]
+}
+</script>
+
+<style>
+
+</style>
